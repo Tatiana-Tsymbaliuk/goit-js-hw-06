62e6d1a3d1bb1a7a0f32e650616401585c805148
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-02-25 19:15:56 +0200

    create task7

--- js/task-07.js
+++ js/task-07.js
@@ -2,18 +2,14 @@ const inputWithId = document.querySelector("#font-size-control");
 
 const textWithId = document.querySelector("#text");
 
-const minNumberInput = Number(inputWithId.getAttribute('min'));
-const maxNumberInput = Number(inputWithId.getAttribute('max'));
 
 
-inputWithId.addEventListener('input', handeleIputChange);
-const number = [];
-function handeleIputChange(event) {
-        for (event = minNumberInput; event <= maxNumberInput; event +1 ){
-                textWithId.style.fontSIze += event;
-                
-        }
-        // if (event.target.value.trim().length =< inputWithId.value.max) {
-              return;  
-        // }
-                };
+inputWithId.addEventListener('input', handleInputChange);
+
+
+function handleInputChange() {
+       
+        textWithId.style.fontSIze = `${inputWithId.value}px`;
+    
+     
+};
