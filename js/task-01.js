7e912ca8f63c01c4f03f7ce3f2a852b371fa68cf
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-02-26 21:39:48 +0200

    corect task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,41 @@
-//console.log(document);
+
 const categoriesWithId = document.querySelector("#categories");
-
 const categoriesEL = document.querySelectorAll('.item');
+
 console.log('Number of categories:', categoriesEL.length);
+const firstEl = categoriesWithId.firstElementChild; 
+
+categoriesEL.forEach(function (firstEl) {
+console.log(`Category: ${firstEl.firstElementChild.textContent}
+Elements: ${firstEl.lastElementChild.children.length}`);
+});
 
 
-const firstEl = categoriesWithId.firstElementChild;        
-console.log("Category:", firstEl.firstElementChild.textContent);
-const firstElTotal = firstEl.lastElementChild;
-console.log("Elements:", firstElTotal.children.length);
 
-const secondEl = categoriesWithId.children[1]; 
-console.log("Category:", secondEl.firstElementChild.textContent);
-const secondElTotal = secondEl.lastElementChild;
-console.log("Elements:", secondElTotal.children.length);
 
-const lastEl = categoriesWithId.lastElementChild;  
-console.log("Category:", lastEl.firstElementChild.textContent);
-const lastElTotal = lastEl.lastElementChild;
-console.log("Elements:", lastElTotal.children.length);
+
+
+
+
+
+
+
+
+
+
+   
+ //const firstEl = categoriesWithId.firstElementChild;    
+// console.log("Category:", firstEl.firstElementChild.textContent);
+ //const firstElTotal = firstEl.lastElementChild;
+// console.log("Elements:", firstElTotal.children.length);
+
+// const secondEl = categoriesWithId.children[1]; 
+// console.log("Category:", secondEl.firstElementChild.textContent);
+// const secondElTotal = secondEl.lastElementChild;
+// console.log("Elements:", secondElTotal.children.length);
+
+// const lastEl = categoriesWithId.lastElementChild;  
+// console.log("Category:", lastEl.firstElementChild.textContent);
+// const lastElTotal = lastEl.lastElementChild;
+// console.log("Elements:", lastElTotal.children.length);
                   
