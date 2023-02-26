11598094294b58e27f723a36fcbf52cd51fe77a3
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-02-26 20:54:14 +0200

    create task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const widgetBtn = document.querySelector('.change-color');
+const widgetBody = document.querySelector('body');
+const widgetColorText = document.querySelector('.color');
+console.log(widgetBody);
+  
+widgetBtn.addEventListener('click', () => {
+  widgetBody.style.backgroundColor = getRandomHexColor();
+  widgetColorText.textContent = getRandomHexColor();
+});
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+ 
+};
